040ad6f2b14c9d781836ee9a00ff2e4880f22978
한주연 <hjy2859@naver.com>
2025-07-19 04:04:07 +0900

    chore: icon 추가

--- src/assets/icons/icon.ts
+++ src/assets/icons/icon.ts
@@ -1,7 +1,11 @@
 import Plus from './svg/plus.svg?react';
+import PlusBackground from './svg/plusBackground.svg';
+import X from './svg/x.svg?react';
 
 export const icons = {
   Plus,
+  PlusBackground,
+  X,
 };
 
 export type IconNameType = keyof typeof icons;
