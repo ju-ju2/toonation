199cbf25d3f9c18a046047a26e24b7623a9d26a5
한주연 <hjy2859@naver.com>
2025-07-19 04:08:20 +0900

    chore: 충전 파트 이넘 생성

---
+++ src/constants/enums.ts
@@ -0,0 +1,10 @@
+export const CHARGE_AMOUNT_MAP = {
+  1: 1000,
+  2: 10000,
+  3: 50000,
+} as const;
+
+export const CHARGE = {
+  TITLE: '충전 금액',
+  CHARGE_AMOUNT: '* 충전 금액은 1,000 캐시 단위로만 결제 가능합니다.',
+} as const;
