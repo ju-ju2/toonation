8438ecee438850a9b078521661ffe4e0e482ac79
한주연 <hjy2859@naver.com>
2025-07-19 14:54:42 +0900

    chore: Card 컴포넌트 props 및 아이콘 추가

--- src/assets/icons/icon.ts
+++ src/assets/icons/icon.ts
@@ -1,3 +1,5 @@
+import Check from './svg/check.svg?react';
+import CheckEmpty from './svg/checkEmpty.svg?react';
 import Plus from './svg/plus.svg?react';
 import PlusBackground from './svg/plusBackground.svg?react';
 import X from './svg/x.svg?react';
@@ -6,6 +8,8 @@ export const icons = {
   Plus,
   PlusBackground,
   X,
+  Check,
+  CheckEmpty,
 };
 
 export type IconNameType = keyof typeof icons;
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,15 +1,41 @@
 import classNames from 'classnames/bind';
+import Icon from '../icon/Icon';
 import styles from './card.module.scss';
 
 const cx = classNames.bind(styles);
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
-  isMain?: boolean;
+interface CardBaseProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+interface MainProps extends CardBaseProps {
+  isMain: true;
+  isSelected?: never;
 }
 
-const Card = ({ isMain, children, className, ...props }: CardProps) => {
+interface NonMainProps extends CardBaseProps {
+  isMain?: false;
+  isSelected?: boolean;
+}
+
+type CardProps = MainProps | NonMainProps;
+
+const Card = ({
+  isMain,
+  isSelected,
+  children,
+  className,
+  ...props
+}: CardProps) => {
   return (
     <div className={cx(['wrapper', { main: isMain }, className])} {...props}>
+      {!isMain && (
+        <div className={cx('icon')}>
+          {isSelected ? (
+            <Icon name="Check" size="md" />
+          ) : (
+            <Icon name="CheckEmpty" />
+          )}
+        </div>
+      )}
       {children}
     </div>
   );
