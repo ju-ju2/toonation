b287895c63ee0e430c6c3f3d409d7fae0edcd2d2
한주연 <hjy2859@naver.com>
2025-07-20 02:08:22 +0900

    style: input 사이즈 및 placeholder 스타일 mixin 구현

--- src/components/page/accountChargeCulture/pinNumber/PinNumber.tsx
+++ src/components/page/accountChargeCulture/pinNumber/PinNumber.tsx
@@ -1,12 +1,24 @@
 import classNames from 'classnames/bind';
 import Content from '@/components/ui/content/Content';
+import PinNumberInput from '@/components/ui/input/pinNumberInput/PinNumberInput';
 import { PIN_NUMBER } from '@/constants/accountChargeCulture';
 import styles from './pinNumber.module.scss';
 
 const cx = classNames.bind(styles);
 
 const PinNumber = () => {
-  return <Content title={PIN_NUMBER.TITLE}></Content>;
+  return (
+    <Content title={PIN_NUMBER.PIN_NUMBER_TITLE}>
+      <PinNumberInput
+        placeholder={PIN_NUMBER.PIN_NUMBER_PLACEHOLDER}
+        size="small"
+        button={{
+          label: PIN_NUMBER.BUTTON_LABEL,
+          onClick: () => alert('핀 번호 입력 완료'),
+        }}
+      />
+    </Content>
+  );
 };
 
 export default PinNumber;
