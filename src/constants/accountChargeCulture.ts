a459409032bea3285606d794db4f842cfb030fb0
한주연 <hjy2859@naver.com>
2025-07-19 23:53:00 +0900

    chore: 상수 생성

---
+++ src/constants/accountChargeCulture.ts
@@ -0,0 +1,20 @@
+export const PIN_NUMBER = {
+  TITLE: '문화상품권 충전',
+  PIN_NUMBER_TITLE: '핀번호 직접 입력',
+  PIN_NUMBER_PLACEHOLDER: '문화상품권 핀번호 입력 (18자리)',
+  PIN_NUMBER_ADD: '핀번호 추가 (5개까지 추가 가능)',
+};
+
+export const TOTAL_CULTURE_AMOUNT = {
+  CHARGE_AMOUNT: '충전 금액',
+  CHARE_FEE: '수수료',
+  TOTAL_AMOUNT: '총 충전 캐시',
+  CHARGE_UNIT: '캐시',
+} as const;
+
+export const AGREEMENT = {
+  TITLE: '유의사항',
+  DESCRIPTION: '* 캐시 충전 완료된 핀번호는 취소 불가합니다.',
+  AGREE: '유의사항을 확인하였으며 문화상품권 충전에 동의합니다.',
+  BUTTON_LABEL: '충전하기',
+} as const;
